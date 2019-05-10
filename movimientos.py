6465a420ddb727bc310a2da987299bc771b80a68
Tu Nombre <you@example.com>
2019-05-09 19:31:22 -0500

    listos2

--- movimientos.py
+++ movimientos.py
@@ -52,6 +52,7 @@ def obtener_nombre_pieza(simbolo):
         return 'No es una pieza'
 
 
+
 # def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
 #     '''
 #     ([][],int,int,int,int)-> [][]: tablero resultante.
