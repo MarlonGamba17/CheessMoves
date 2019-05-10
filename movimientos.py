cb09747c104929b5e0e4091448d562a95cde5250
Tu Nombre <you@example.com>
2019-05-09 19:14:19 -0500

    Insercciòn de funciòn Torre del profe.

--- movimientos.py
+++ movimientos.py
@@ -37,3 +37,70 @@ def obtener_nombre_pieza(simbolo):
         return 'Rey ' + tipo
     else:
         return 'No es una pieza'
+
+
+
+def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
+    # Valido que se este moviendo una torre
+    if tablero[y_inicial][x_inicial].lower() == 't':
+
+        # Valido que se este moviendo en y
+        if x_inicial == x_final and y_inicial != y_final:
+
+            # Si me muevo hacia abajo
+            if y_inicial < y_final:
+                y_auxiliar = y_inicial + 1
+            # Si no me muevo hacia abajo
+            else:
+                y_auxiliar = y_inicial - 1
+            # Reviso  el camino por obstaculos
+            for y in range(y_auxiliar, y_final):
+                if tablero[y][x_inicial] != ' ':
+                    raise Exception('No hay camino para mover la torre')
+
+        # Valido el movimiento en x
+        elif x_inicial != x_final and y_inicial == y_final:
+            # validar si me muevo a la derecha
+            if x_inicial < x_final:
+                x_auxiliar = x_inicial + 1
+            else:
+                x_auxiliar = x_inicial - 1
+
+            # Reviso el camino por obtaculos
+            for x in range(x_auxiliar, x_final):
+                if tablero[x][y_inicial] != ' ':
+                    raise Exception('No hay camino para mover la torre')
+
+
+        else:
+            raise Exception('Movimiento invalido para la torre')
+
+        if tablero[y_final][x_inicial] == ' ' \ or (tablero[y_inicial][x_inicial].islower() != tablero[y_final][x_inicial].islower()):
+            tablero[y_final][x_inicial] = tablero[y_inicial][x_inicial]
+            tablero[y_inicial][x_inicial] = ' '
+        else:
+            raise Exception('No puedo comer mis propias piezas')
+
+    else:
+        raise Exception('La pieza en x = {0} y={1} no es una torre'.format(x_inicial,y_inicial))
+    return tablero
+
+def mover_reina(tablero, x_inicial, y_inicial, x_final, y_final):
+    '''
+    ([][],int,int,int,int)-> [][]: tablero resultante.
+    :param tablero: [][]: matriz con la posicion de las fichas
+    :param x_inicial: int: entero indocando posicion en x inicial
+    :param y_inicial: int: entero indocando posicion en y inicial
+    :param x_final: int: entero indocando posicion en x final
+    :param y_final: int: entero indocando posicion en y final
+    :return: [][] tablero resultante
+    '''
+    # Valido que se este moviendo una torre
+    if tablero[y_inicial][x_inicial].lower() == 't':
+
+    ficha = tablero[x_inicial][x_inicial]
+
+    if (mover_torre(tablero,x_inicial,y_inicial,x_final,y_final)):
+        print('se realizo el movimiento efectivamente')
+
+print(mover_reina(tablero, 1, 5, 1, 7))
